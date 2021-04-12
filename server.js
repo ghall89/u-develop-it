8f8d45d4ec49ea9548082c6a39966292e424c010
Graham Hall <ghall89@me.com>
2021-04-12 16:16:28 +0000

    connect database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,12 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.json({
-		message: 'Hello World!'
-	});
+// connect database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		user: 'root',
+		password: 'Dundee89',
+		database: 'election'
+	},
+	console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
 });
 
+// catchall route
 app.use((req, res) => {
 	res.status(404).end();
 });
